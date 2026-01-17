c2cedb49ada8d9028238827ca230bcdcf482f3c5
ijuhygtg <luisitocarranzito19@gmaol.com>
2026-01-16 20:29:56 -0700

    Primera version de pruebaweb

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('bat-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Mensaje enviado a la Baticueva. Alfred te contactará pronto.');
+});
